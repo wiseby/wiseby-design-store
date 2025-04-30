81d8fc71af698332ef7f34b5716fec7a421d648a
Lucas Wiseby <l.wiseby@gmail.com>
2025-04-30 14:38:16 +0200

    redirect to productpage

--- store/auth.py
+++ store/auth.py
@@ -58,7 +58,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('product.index'))
 
         flash(error)
 
